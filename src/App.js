14b0de866ae981e28a3bd7150d30430c1f9c9f5a
masTech Developer <ashir342@gmail.com>
2020-08-01 12:33:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { Navbar } from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <Header />
+      <div className='container'>
+        <Balance />
+
+        <IncomeExpense />
+
+        <AddTransaction />
+
+        <TransactionList />
+      </div>
     </div>
   );
 }
