2393a674d357c179acef346e1f3806787dddaf98
masTech Developer <ashir342@gmail.com>
2020-08-01 22:19:35 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <div>
-      <h1>Expense Tracker</h1>
+      <h1 className='header'>Expense Tracker</h1>
     </div>
   );
 };
