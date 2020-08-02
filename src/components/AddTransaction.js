7c1c67b76d1012896a3e56c5c54673cb78613774
masTech Developer <ashir342@gmail.com>
2020-08-02 18:14:33 +0500

    Update AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../contextAPI/GlobalState';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTrans } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTrans = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTrans(newTrans);
+  };
+
   return (
     <div>
       <h3>Add a New Transaction</h3>
-      <form id='form'>
+      <form onSubmit={onSubmit}>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
-          <input type='text' id='text' placeholder='Enter text...' />
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
         </div>
         <div className='form-control'>
           <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' id='amount' placeholder='Enter amount...' />
+          <input
+            type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter amount...'
+          />
         </div>
         <button className='btn'>Add transaction</button>
       </form>
